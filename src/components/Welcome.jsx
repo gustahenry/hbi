cf09cc9f23bfa5c582c03e88d51888704bcc332b
gustavo henrique <ghgoncalves430@gmail.com>
2024-08-18 17:30:49 -0300

    criado responsividade do componente Welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Welcome = ({ onClick }) => {
   return (
     <div className="flex flex-col justify-center items-center h-screen space-y-8 text-white">
-        <h1 className='text-6xl font-semibold'>Busca CEP HBI</h1>
+        <h1 className='lg:text-6xl text-5xl font-semibold'>Busca CEP HBI</h1>
         <p className='text-2xl'>18/08/2024</p>
         <button
          onClick={onClick}
