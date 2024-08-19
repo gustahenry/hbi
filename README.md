ab28a976e3dd5408beb5c77bab149cd414270c89
gustavo henrique <ghgoncalves430@gmail.com>
2024-08-19 12:22:58 -0300

    correção do bug resposivo da tabela e inclusão do readme

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
-# React + Vite
+# README: Busca Cep HBI
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Descrição
 
-Currently, two official plugins are available:
+**Busca Cep HBI** é um projeto React criado para buscar informações de CEPs utilizando a API dos Correios. O projeto permite que os usuários realizem buscas por CEPs, preenchem campos automaticamente com os dados retornados e visualizam os resultados em uma tabela.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Tecnologias Utilizadas
+
+- **Tailwind CSS**: Para facilitar o desenvolvimento e garantir a responsividade do projeto, além de criar consistência em todo o design.
+- **react-input-mask**: Biblioteca para aplicar máscaras em formulários, melhorando a visualização dos dados inseridos e mantendo o número de caracteres..
+- **react-hook-form**: Facilitando a criação e validação de campos para o formúlario.
+
+## Estrutura
+
+- **Componentes**: Foi ultilizado componentes para facilitar a manutenção além de reutilização.
+
+
+## Instalação
+
+### Configuração do Ambiente
+
+1. **Instalar Dependências**
+
+   ```bash
+   npm install
+   npm run dev
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -8,29 +8,32 @@ const Table = ({ data }) => {
   const headers = Object.keys(data[0]);
 
   return (
-    <div className="overflow-x-auto mt-6">
-      <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
-        <thead>
-          <tr className="bg-gray-100 border-b border-gray-200">
-            {headers.map((header) => (
-              <th key={header} className="px-4 py-2 text-left text-gray-600 font-semibold text-xs md:text-sm lg:text-base">
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, index) => (
-            <tr key={index} className="border-b border-gray-200">
+    <div className="mt-6">
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
+          <thead className="hidden sm:table-header-group">
+            <tr className="bg-gray-100 border-b border-gray-200">
               {headers.map((header) => (
-                <td key={header} className="px-4 py-2 text-gray-700 text-xs md:text-sm lg:text-base">
-                  {row[header]}
-                </td>
+                <th key={header} className="px-4 py-2 text-left text-gray-600 font-semibold text-xs md:text-sm lg:text-base">
+                  {header}
+                </th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index} className="border-b border-gray-200 flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                {headers.map((header) => (
+                  <td key={header} className="px-4 py-2 text-gray-700 text-xs md:text-sm lg:text-base border-b border-gray-200">
+                    <span className="block sm:hidden font-semibold">{header}:</span>
+                    {row[header]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
